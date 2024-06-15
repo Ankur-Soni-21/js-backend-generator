ba59049e421aec80e3f96436e34ad02dfc1fcb6c
ankur soni <an2112soni@gmail.com>
2024-06-16 01:17:53 +0530

    init

---
+++ README.md
@@ -0,0 +1,105 @@
+# JS Backend Project Creator
+
+This is a CLI tool to create a backend project structure with commonly used folders and files. It supports different frameworks like Express, Express with MongoDB, and Express with SQL.
+
+## Usage
+
+### Using `npx`
+
+You can create a new project structure using `npx` without having to install the package globally:
+
+```sh
+npx js-backend <project-name>
+```
+
+For example:
+
+```sh
+npx js-backend MyApiProject
+```
+
+### Trying It Locally
+
+1. **Clone the repository or download the script:**
+
+    ```sh
+    git clone <repository-url>
+    cd js-backend
+    ```
+
+2. **Install the dependencies:**
+
+    ```sh
+    npm install
+    ```
+
+3. **Run the script locally:**
+
+    ```sh
+    node createProject.js <project-name>
+    ```
+
+    For example:
+
+    ```sh
+    node createProject.js MyApiProject
+    ```
+
+## Packages Used
+
+* **ora**: For displaying a spinner and making the CLI user-friendly.
+* **chalk**: For adding colors to the CLI output, making it more readable and engaging.
+* **inquirer**: For prompting the user for input during the execution of the script.
+* **fs**: Node.js file system module used for creating directories and files.
+* **path**: Node.js path module used for handling and transforming file paths.
+
+## Features
+
+* **Interactive CLI**: Prompts the user for a project name and framework choice.
+* **Multiple Framework Support**: Allows creating projects using different frameworks like Express, Express with MongoDB, and Express with SQL.
+* **Automatic Directory and File Creation**: Sets up a predefined project structure with common folders and files.
+* **Package.json Generation**: Generates a `package.json` file based on the selected framework and installs the necessary dependencies.
+
+## Project Structure
+
+The following directory structure is created:
+
+```
+<project-name>/
+├── src/
+│   ├── controllers/
+│   ├── models/
+│   ├── routes/
+│   ├── services/
+│   ├── middlewares/
+│   ├── config/
+│   ├── utils/
+│   ├── index.js
+├── tests/
+├── .gitignore
+├── README.md
+├── package.json
+```
+
+## Example
+
+Running the script with the project name `MyApiProject` and selecting `Express` as the framework:
+
+```sh
+npx js-backend MyApiProject
+```
+
+This will create a project structure with the following commands to start:
+
+```sh
+cd MyApiProject
+npm install
+```
+
+## Author
+
+Ankur Soni
+
+## License
+
+This project is licensed under the MIT License.
---
+++ index.js
@@ -0,0 +1,169 @@
+#!/usr/bin/env node
+
+// Importing required modules
+import ora from 'ora';
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import fs from 'fs';
+import path from 'path';
+
+const directories = [
+    'src',
+    'src/controllers',
+    'src/models',
+    'src/routes',
+    'src/services',
+    'src/middlewares',
+    'src/config',
+    'src/utils',
+    'tests',
+];
+
+// List of files to create
+const files = [
+    'src/index.js',
+    'src/controllers/.gitkeep',
+    'src/models/.gitkeep',
+    'src/routes/.gitkeep',
+    'src/services/.gitkeep',
+    'src/middlewares/.gitkeep',
+    'src/config/.gitkeep',
+    'src/utils/.gitkeep',
+    'tests/.gitkeep',
+    '.gitignore',
+    'README.md',
+];
+
+
+// Function to ask for project name if not provided
+async function getProjectName() {
+    if (process.argv.length < 3) {
+        const answers = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'projectName',
+                message: 'Please provide a project name:',
+            },
+        ]);
+        return answers.projectName;
+    }
+    return process.argv[2];
+};
+
+
+// the MAIN function to create the project structure
+async function createBackendStructure() {
+    // Get project name
+    const projectName = await getProjectName();
+    // Create project directory
+    const projectDir = path.join(process.cwd(), projectName);
+    // Check if project already exists
+    if (fs.existsSync(projectDir)) {
+        console.log(`Project '${projectName}' already exists!`);
+        process.exit(1);
+    }
+    // Select Framework
+    const framework = await selectFramework();
+
+    // Print project name
+    console.log(`Creating project: ${chalk.hex('#FFD700')(projectName)}`); // Using gold color for project name
+
+    const spinnerCreateDirectories = ora('Creating project directories').start();
+    directories.forEach((dir) => {
+        const dirPath = path.join(projectDir, dir);
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, { recursive: true });
+        }
+    });
+    spinnerCreateDirectories.succeed('Directories created.');
+
+    // Create files
+    const spinnerCreateFiles = ora('Creating project files').start();
+    files.forEach((file) => {
+        const filePath = path.join(projectDir, file);
+        if (!fs.existsSync(filePath)) {
+            fs.writeFileSync(filePath, ''); // Empty file for .gitkeep
+        }
+    });
+    spinnerCreateFiles.succeed('Files created.');
+
+    // Generate package.json file
+    const spinnerGeneratePackageFile = ora('Generating package.json file').start();
+    await generatingPackageFile(framework, projectDir, projectName);
+    spinnerGeneratePackageFile.succeed('package.json file generated.');
+
+    // Install packages
+    console.log(`Project structure for '${projectName}' created successfully!`);
+    console.log(`\nRun the following commands to start the project:`);
+    console.log(`cd ${projectName}`);
+    console.log(`npm install`);
+}
+
+
+const selectFramework = async () => {
+    const { framework } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'framework',
+            message: 'SELECT THE FRAMEWORK:',
+            choices: [
+                { name: 'Express', value: 'express' },
+                { name: 'Express + Mongo', value: 'expressMongo' },
+                { name: 'Express + SQL', value: 'expressSQL' },
+                // { name: 'Custom', value: 'custom' },
+            ],
+            transformer: (color) => {
+                return chalk.hex('#FFA07A')(color); // Using light salmon color for choices
+            },
+        },
+    ]);
+
+    return framework;
+};
+
+
+async function createPackageJson(projectName, projectDir, dependencies) {
+    const data = {
+        name: projectName,
+        version: "1.0.0",
+        description: `${projectName} project`,
+        scripts: {
+            test: "echo \"No tests implemented yet.\"",
+            start: "node src/index.js",
+        },
+        dependencies: dependencies,
+    };
+
+    const filePath = projectDir + "/package.json";
+
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
+        console.log(`package.json created for project '${projectName}'`);
+    } catch (error) {
+        console.error("Error creating package.json:", error);
+    }
+}
+
+
+async function generatingPackageFile(framework, projectDir, projectName) {
+
+    let dependencies = {};
+
+    switch (framework) {
+        case 'express':
+            dependencies['express'] = 'latest';
+            break;
+        case 'expressMongo':
+            dependencies['express'] = 'latest';
+            dependencies['mongoose'] = 'latest';
+            break;
+        case 'expressSQL':
+            dependencies['express'] = 'latest';
+            dependencies['sequelize'] = 'latest';
+            break;
+    }
+
+    createPackageJson(projectName, projectDir, dependencies);
+};
+
+createBackendStructure();
